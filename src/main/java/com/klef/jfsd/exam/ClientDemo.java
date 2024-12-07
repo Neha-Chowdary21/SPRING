8a5e619f801a83d1e4d162af270233854ffb53c2
Neha-chowdary21 <nehacm2003@gmail.com>
2024-12-07 08:36:01 +0530

    V1.0

---
+++ src/main/java/com/klef/jfsd/exam/ClientDemo.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.exam;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println("Employee Details: " + employee);
+        Course course = (Course) context.getBean("course");
+        System.out.println("Course Details: " + course);
+    }
+}
